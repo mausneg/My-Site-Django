c4e61e606fb0e8d045400bd01d239b1ed316c409
mausneg <maulanasuryanegara123@gmail.com>
2025-11-05 20:42:21 +0800

    add urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+user = {
+    "username": "mausneg",
+    "profile_path": ""
+}
+
+def index(request):
+    return render(request, "blog/index.html", {
+        "user": user
+    })
+
+def posts(request):
+    return HttpResponse("posts")
+
+def post_detail(request, slug):
+    return HttpResponse(slug)
