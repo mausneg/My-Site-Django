6836b730342c0c6e9d601523fe7a4c02b95c2345
mausneg <maulanasuryanegara123@gmail.com>
2025-11-06 11:09:54 +0800

    add link page for post and posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="landing-page"),
-    path("posts", views.all_posts, name="all-posts-page"),
+    path("", views.index, name="starting-page"),
+    path("posts", views.all_posts, name="posts-page"),
     path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
 ]
