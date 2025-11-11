ab42025de577ae4d82037452b6d6318373ef6501
mausneg <maulanasuryanegara123@gmail.com>
2025-11-11 15:46:39 +0800

    add tags and author email at post detail page

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,22 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from datetime import date
-from django.http import Http404
-from .models import Post, Author, Tag
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-posts_data = Post.objects.all().order_by("-date")
 
 def index(request):
-    latest_post = posts_data[:3]
-    print(latest_post)
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
-        "posts": latest_post
+        "posts": latest_posts
     })
 
 def posts(request):
+    posts_data = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html",{
         "posts": posts_data
     })
 
 def post_detail(request, slug):
-    selected_post = next((post for post in posts_data if post.slug == slug) , None)
-    if selected_post is None:
-        raise Http404()
+    selected_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-        'post': selected_post
+        'post': selected_post,
+        "tags": selected_post.tag.all()
     })
